66dd6a9991831193f9d04170213e1d8e2f3532d4
NikJensen97 <npjjensen@hotmail.com>
2023-03-28 21:48:07 -0700

    char counter now displays correctly

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+
+  $('textarea').on('keypress', function() {
+    current = $('.counter');
+    var characterCount = $(this).val().length
+    current.text(139 - characterCount);
+    
+    if (characterCount > 139) {
+      current.css('color', 'red');
+    } else {
+      current.css('color', 'black');
+    }
+  
+    })
+  /*
+  const textarea = document.getElementById("textbox");
+
+  textarea.addEventListener("keypress", function() {
+    console.log(this);
+  }); */
+});
