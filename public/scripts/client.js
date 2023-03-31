2ba21e15e02c62a53d7c01b5cfe1f88080137233
NikJensen97 <npjjensen@hotmail.com>
2023-03-30 22:58:33 -0700

    no longer sends bad tweets to json

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -35,19 +35,23 @@ $(document).ready(function () {
   const renderTweets = function (tweets) {
     $('#tweet-container').empty();
     // loops through tweets
+    
     for (let y = 0; y < tweets.length; y++) {
       let $tweet = createTweetElement(tweets[y]);
+      if ($tweet) {
       $('#tweet-container').prepend($tweet);
+      }
     }
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
   }
 
   const createTweetElement = function (tweet) {
-   /* if (tweet["content"]["text"].length > 140 || tweet["content"]["text"] === '') {
-      alert('message is invalid');
+    /*if (tweet["content"]["text"].length > 140) {
+      $('.error').slideDown("slow");
       return;
-    } */
+    }*/
+    let safeword = encode(tweet["content"]["text"]);
     
     let $tweet = $(`
       <article>
@@ -56,7 +60,7 @@ $(document).ready(function () {
           <h3>${tweet["user"]["name"]}</h3>
           <p class = "account">${tweet["user"]["handle"]}</p>
         </header>
-        <p class = "message">${encodeURI(tweet["content"]["text"])}</p>
+        <p class = "message">${safeword}</p>
         <footer>
           <p class = "date">${timeago.format(tweet["created_at"])}</p>
           <i class="fa-solid fa-heart"></i>
@@ -65,8 +69,10 @@ $(document).ready(function () {
       </footer>
       </article>
       `)
+      // try this tmr ?? $('message').text(tweet["content"]["text"]);
     return $tweet;
   }
+
   $.ajax('http://localhost:8080/tweets', { method: 'GET' })
   .then(function (json) {
     renderTweets(json);
@@ -78,21 +84,38 @@ $(document).ready(function () {
       renderTweets(json);
     }
   )};
+  // function to escape malicious tweets
+  function encode(string){
+    return string.replace(/[\x26\x0A\<>'"]/g,function(string){return"&#"+string.charCodeAt(0)+";"})
+    }
   
   // $('#tweet-container').append($tweet);
   //renderTweets(data);
   $form.on('submit', (event) => {
     event.preventDefault();
+    $('.error').fadeOut("slow");
+    let name = $("#tweet-text").val();
+    if (name.length > 140) {
+      $('.error').text('Error: Tweet is too long');
+      $('.error').slideDown("slow");
+      return;
+    }
+    if (name.length === 0) {
+      $('.error').text('Error: No tweet entered');
+      $('.error').slideDown("slow");
+      return;
+    }
+    
     const urlencoded = $form.serialize();
-    console.log(urlencoded);
-
+    // console.log(urlencoded);
+    
 
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: urlencoded
     }).then((response) => {
-       console.log(response);
+       // console.log(response);
        loadtweets();
     });
 
