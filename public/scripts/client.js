781d4531c58d86c1b98de092896c0c2307141b98
NikJensen97 <npjjensen@hotmail.com>
2023-04-04 23:40:17 -0700

    now resets the textarea and updates the counter on successful submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-/*const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-] */
+
 
 $(document).ready(function () {
 
@@ -42,15 +18,9 @@ $(document).ready(function () {
       $('#tweet-container').prepend($tweet);
       }
     }
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
   }
 
   const createTweetElement = function (tweet) {
-    /*if (tweet["content"]["text"].length > 140) {
-      $('.error').slideDown("slow");
-      return;
-    }*/
     let safeword = encode(tweet["content"]["text"]);
     
     let $tweet = $(`
@@ -69,17 +39,16 @@ $(document).ready(function () {
       </footer>
       </article>
       `)
-      // try this tmr ?? $('message').text(tweet["content"]["text"]);
     return $tweet;
   }
 
-  $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+  $.ajax('/tweets', { method: 'GET' })
   .then(function (json) {
     renderTweets(json);
   });
   const $form = $('#textbox');
   const loadtweets = function () {
-    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+    $.ajax('/tweets', { method: 'GET' })
     .then(function (json) {
       renderTweets(json);
     }
@@ -89,8 +58,6 @@ $(document).ready(function () {
     return string.replace(/[\x26\x0A\<>'"]/g,function(string){return"&#"+string.charCodeAt(0)+";"})
     }
   
-  // $('#tweet-container').append($tweet);
-  //renderTweets(data);
   $form.on('submit', (event) => {
     event.preventDefault();
     $('.error').fadeOut("slow");
@@ -107,18 +74,19 @@ $(document).ready(function () {
     }
     
     const urlencoded = $form.serialize();
-    // console.log(urlencoded);
     
-
     $.ajax({
       method: 'POST',
       url: '/tweets',
       data: urlencoded
     }).then((response) => {
-       // console.log(response);
        loadtweets();
+       
     });
-
+    document.getElementById("textbox").reset();
+    current = $('.counter');
+      let characterCount = $(this).val().length
+      current.text(140 - characterCount);
   });
   
   
